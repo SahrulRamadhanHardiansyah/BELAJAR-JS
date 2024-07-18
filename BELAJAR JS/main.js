6ad1db53f9cab05a3ee37e7c2ca09abd2df11f04
Kvmen <sahrulrmdh3@gmail.com>
2024-07-18 21:07:54 +0700

    Add files via upload

---
+++ BELAJAR JS/main.js
@@ -0,0 +1,57 @@
+// let saldoawal = 100000;
+// alert(`saldo kamu adalah ${saldoawal}`);
+// let deposit = prompt("masukan jumlah deposit yang diinginkan... ");
+// deposit = parseFloat(deposit);
+// let saldoakhir = saldoawal + deposit;
+// alert(`saldo akhir kamu adalah... ${saldoakhir} `);
+
+// let hari = new Date().getDay();
+// const namaHari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+
+// console.log(`Hari ini adalah hari ${namaHari[hari]}`);
+
+// const uang = 20
+// const hutang = 2000
+// tambahUang = () => {
+//     return uang + hutang
+
+// }
+
+// const saldoAkhir = tambahUang()
+
+// console.log(saldoAkhir)
+
+
+
+// const sisi = prompt('masukan sisi... ')
+
+
+// luas = () => {
+//     return sisi * sisi 
+// }
+
+// const hasil = luas()
+
+
+// console.log(`hasilnya adalah ${hasil}`)
+// alert(`hasilnya adalah ${hasil}`)
+
+
+const arraySaya = ['1', '2']
+
+const angka = arraySaya.includes('99')
+const posisi = arraySaya.indexOf("99")
+
+if (angka) {
+    console.log(
+        `ada di index ke - ${posisi}`
+    )
+} else {
+    console.log(
+        `saya tidak tahu`
+    )
+}
+
+
+
+
